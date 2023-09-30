002aaa2052cc641e44136436ba8e06c925065d19
kirillkom <kk7453603@gmail.com>
2023-09-30 21:49:43 +0300

    Задание 2 сделано (альфа версия)

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import openpyxl
+from django.http import HttpResponse
+import datetime
+from robots.models import Robot
+from django.db.models import Count
 
-# Create your views here.
+
+def export_production(request):
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    robot_models = Robot.objects.filter(created__gte=str(last_week), created__lte=str(datetime.date.today())).values(
+        'model', 'version').annotate(count=Count('id'))
+    # суть запроса на SQL: SELECT model,version,COUNT(*) FROM robots_robot GROUP BY serial,model
+
+    wb = openpyxl.Workbook()
+    check_set = set()  # Костыль для неработающего DISTINCT
+    for robot_model in robot_models:
+        model = robot_model['model']
+        if not model in check_set:
+            check_set.add(model)
+        else:
+            continue
+        wt = wb.create_sheet(title=model)
+        wt['A1'] = 'Модель'
+        wt['B1'] = 'Версия'
+        wt['C1'] = 'Количество за неделю'
+
+        robot_production = robot_models.filter(model=model)
+        row = 2
+        for rp in robot_production:
+            wt.cell(row=row, column=1, value=rp['model'])
+            wt.cell(row=row, column=2, value=rp['version'])
+            wt.cell(row=row, column=3, value=rp['count'])
+            row += 1
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=robot_production.xlsx'
+    wb.save(response)
+    return response
--- robots/models.py
+++ robots/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-import re
 
-
-def valid_models(value1,value2):
-    pattern = r'^[A-Z0-9]{2}$'
-    if not re.match(pattern, value1) or not re.match(pattern,value2):
-        raise ValidationError("Invalid pattern value")
 
 
 
@@ -16,9 +9,3 @@ class Robot(models.Model):
     version = models.CharField(max_length=2, blank=False, null=False)
     created = models.DateTimeField(blank=False, null=False)
 
-    def clean(self):
-        valid_models(self.model,self.version)
-
-    @property
-    def __str__(self):
-        return self.model
